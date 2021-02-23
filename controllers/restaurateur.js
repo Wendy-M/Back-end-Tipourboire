54e7628f34af043c2f46b82e6ab97f76e5069dbc
wendy <wendymontagnon@gmail.com>
2021-02-23 10:05:11 +0100

    mdp mail

--- controllers/restaurateur.js
+++ controllers/restaurateur.js
@@ -878,10 +878,13 @@ const restaurateurController = {
         });
         Restaurateur.findOne({ email: email }, (error, user) => {
           let transporter = nodemailer.createTransport({
-            service: "gmail",
+            pool: true,
+            host: "authsmtp.securemail.pro",
+            port: 465,
+            secure: true, // use TLS
             auth: {
-              user: process.env.EMAIL || "tiptotest@gmail.com",
-              pass: process.env.PASSWORD || "!TTTmdp51!",
+              user: "contact@tipourboire.com",
+              pass: "Vitrine20203T/",
             },
           });
 
@@ -889,9 +892,9 @@ const restaurateurController = {
             "https://back-end.osc-fr1.scalingo.io/restaurateur/autorisation-password?email=" +
             user.email;
           let mailOptions = {
-            from: "tiptotest@gmail.com",
+            from: "contact@tipourboire.com",
             to: req.body.email,
-            subject: "Nodemailer - Test",
+            subject: "Récupértion de votre mot de passe",
             html:
               '<header  style=" background-color:#f4a521"> <h1 style="color: white; font-size: 30px; text-align:center; padding:10px">TIPOURBOIRE</h1></header> <p style=" padding:15px; text-align:center; font-size:18px; font-family:arial">Vous avez demandé la modification de votre mot de passe. <br/> Cliquez sur le lien ci-dessous pour confirmer votre demande <br/> <br/>  <a style=" margin-top:15px; text-decoration:none; color: #f4a521; font-weight:bold; font-size:23px; font-family:arial" href=' +
               link +
