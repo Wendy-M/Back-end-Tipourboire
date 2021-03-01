b326f3c0dde7fd4a82b975d32026d85135cb7915
wendy <wendymontagnon@gmail.com>
2021-03-01 10:23:38 +0100

    cors

--- middlewares/cors/corsCli.js
+++ middlewares/cors/corsCli.js
@@ -4,13 +4,20 @@
  */
 
  //let ALLOWORIGIN= ["https://client.osc-fr1.scalingo.io/" , "https://restaurant.osc-fr1.scalingo.io/", "https://serveur.osc-fr1.scalingo.io/"]
+
+    
+let allowedOrigins = ["https://client.osc-fr1.scalingo.io/" , "https://restaurant.osc-fr1.scalingo.io/", "https://serveur.osc fr1.scalingo.io/"]
 const cors = {
     handle: (req, res, next) => {
-     
+
+      let origin = req.get('origin')
+      
+      res.header(
+        "Access-Control-Allow-Origin", allowedOrigins.inlcudes(origin) ? origin : "null"      
+      );
       res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-        "Access-Control-Allow-Origin", "*"
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
       );
       res.header("Access-Control-Allow-Credentials", true);
       if (req.method === "OPTIONS") {
